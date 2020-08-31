5160415d47b4eba08a5b14f4306475fed9cd29e7
cjdufault <dufaultconnor@gmail.com>
2020-08-31 12:55:26 -0500

    added comments to student.py about differences between regular classes and dataclasses

--- student.py
+++ student.py
@@ -3,6 +3,10 @@ from dataclasses import dataclass
 
 @dataclass
 class Student:
+    """ 
+    Much less typing to do here. It also allows you to explicitly define the 
+    types of fields, which is really nice to have.
+    """
     name: str
     college_id: int
     gpa: float
@@ -13,8 +17,19 @@ def main():
     bob = Student("Bob", 54321, 3.0)
     
     print(alice.name)
-    print(bob.college_id)
+    print(alice.college_id)
+    print(alice.gpa)
     
+    print(bob.name)
+    print(bob.college_id)
+    print(bob.gpa)
+    
+    
+    """
+    The default __str__ method for dataclasses puts the name of the class in
+    the string, which may or may not be what you want it to do. You can always
+    override it, though.
+    """
     print(alice)
     print(bob)
     
